96a13393a5001ad6c21fa599f9775452165b44c3
SilentXBT <larrysongift@gmail.com>
2025-11-01 07:22:33 -0700

    component

--- 2_GymBro/src/components/Grid.jsx
+++ 2_GymBro/src/components/Grid.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { workoutProgram as training_plan} from "../utils/index.js";
 
 export const Grid = () => {
     return (
-        <div>Grid</div>
+        <div className="training-grid-plan">
+            {Object.keys(training_plan).map((workout, workoutIndex) => {
+                return (
+                    <button key={workoutIndex}>
+                        <div className="plan-card-header">
+                            <p>Day {((workoutIndex / 8) <= 1) ? '0' + (workoutIndex + 1 ) : workoutIndex + 1}</p>
+
+                        </div>
+
+                    </button>
+                )
+
+            })}
+
+
+
+          </div>
+
+
+
+
+
+
+
     )
 }
