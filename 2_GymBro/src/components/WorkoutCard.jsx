30a4df41c2da27d1df3ef5d56fb117c0f2dcce38
SilentXBT <larrysongift@gmail.com>
2025-11-01 15:11:15 -0700

    workoutCard

--- 2_GymBro/src/components/WorkoutCard.jsx
+++ 2_GymBro/src/components/WorkoutCard.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
-export const WorkoutCard = () => {
+export const WorkoutCard = (props) => {
+    const {trainingPlan, workoutIndex, type, dayNum, icon} = props
+
+    const { warmup, workout } = trainingPlan || {}
+
     return (
-        <div>WorkoutCard</div>
+        <div className="workout-container">
+            <div className="workout-card card">
+                <div className="plan-card-header">
+                    <p>Day {dayNum}</p>
+                    {icon}
+                </div>
+                <div className="plan-card-header">
+                    <h2><b>{type} WorkOut</b></h2>
+
+                </div>
+
+            </div>
+
+
+
+            <div className="workout-grid">
+                <div className="exercise-name">
+                    <h4>Warmup</h4>
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 className="weight-input">Max Weight</h6>
+            </div>
+
+
+        </div>
     )
 }
