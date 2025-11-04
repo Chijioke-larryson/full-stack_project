885945bfcf51190a81a4faa00f1ffa73513a2d68
SilentXBT <larrysongift@gmail.com>
2025-11-03 17:35:05 -0800

    workoutCard

--- 2_GymBro/src/components/WorkoutCard.jsx
+++ 2_GymBro/src/components/WorkoutCard.jsx
@@ -34,11 +34,15 @@ export const WorkoutCard = (props) => {
                 return (
                     <React.Fragment key={warmupIndex}>
                         <div className="exercise-name">
-                            <p>{warmupExercise + 1}. {warmupExercise.name}</p>
-                            <p className="exercise-info">{warmupExercise.sets}</p>
-                            <p>{warmupExercise + 1}. {warmupExercise.reps}</p>
+                            <p>{warmupIndex + 1}. {warmupExercise.name}</p>
+                            <button className="help-icon">
+                                <i className="fa-regular fa-circle-question"/>
+                            </button>
 
                         </div>
+                        <p className="exercise-info">{warmupExercise.sets}</p>
+                        <p className="exercise-info"> {warmupExercise.reps}</p>
+                        <input className="weight-input" placeholder="N/A" disabled/>
 
                     </React.Fragment>
                 )
@@ -46,6 +50,46 @@ export const WorkoutCard = (props) => {
             })}
 
         </div>
+
+
+            <div className="workout-grid">
+                <div className="exercise-name">
+                    <h4>Workout</h4>
+
+
+                </div>
+                <h6>Sets</h6>
+                <h6>Reps</h6>
+                <h6 className="weight-input">Max Weight</h6>
+                {workout.map((workoutExercise, workoutIndex) => {
+
+                    return (
+                        <React.Fragment key={workoutIndex}>
+                            <div className="exercise-name">
+                                <p>{workoutIndex + 1}. {workoutExercise.name}</p>
+                                <button className="help-icon">
+                                    <i className="fa-regular fa-circle-question" />
+                                </button>
+
+                            </div>
+                            <p className="exercise-info">{workoutExercise.sets}</p>
+                            <p className="exercise-info"> {workoutExercise.reps}</p>
+                            <input className="weight-input" placeholder="14" />
+
+                        </React.Fragment>
+                    )
+
+                })}
+
+            </div>
+
+
+            <div className="workout-buttons">
+                <button>Save & Exit</button>
+                <button disabled={true}>Complete</button>
+
+            </div>
+
 
 
         </div>
